b737dbe1bd8190fb1fd3b7598ae01b788a14b180
Hieukk <hieuhv@gameestudio.com>
2023-01-31 17:26:36 +0700

    update popup lose

--- Assets/_Root/_GUI/Popup/Modun/PopupLose/Scripts/PopupLose.cs
+++ Assets/_Root/_GUI/Popup/Modun/PopupLose/Scripts/PopupLose.cs
@@ -24,6 +24,7 @@ namespace Gamee.Hiuk.Popup
         public void BackToHome()
         {
             actionBackToHome?.Invoke();
+            Close();
         }
         public void Skip()
         {
